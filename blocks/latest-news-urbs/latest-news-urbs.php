6dd7037f52867acc85e4b2384d17930e2e4e940d
William Kunitake <william@bulkdesign.com.br>
2024-05-02 08:34:34 -0700

    Layout adjustments

--- blocks/latest-news-urbs/latest-news-urbs.php
+++ blocks/latest-news-urbs/latest-news-urbs.php
@@ -32,44 +32,46 @@
         ?>
 
         <?php if ( $the_query->have_posts() ) : ?>
-            <div class="latest-news-urbs-list-wrapper">
-                <div class="swiper news-carousel-list" data-loop="<?php the_field( 'enable_carousel_loop' ); ?>" data-autoplay="<?php the_field( 'enable_carousel_autoplay' ); ?>" data-animation>
+            <div class="latest-news-urbs-wrapper">
+                <div class="swiper news-carousel" data-loop="<?php the_field( 'enable_carousel_loop' ); ?>" data-autoplay="<?php the_field( 'enable_carousel_autoplay' ); ?>" data-animation>
                     <div class="swiper-wrapper">
                         <?php while ( $the_query->have_posts() ) : ?>
                             <?php $the_query->the_post(); ?>
+
+                            <?php $featured_image = get_the_post_thumbnail_url( get_the_ID(), 'large' ); ?>
+                            <?php $featured_image_alt = get_post_meta($featured_image , '_wp_attachment_image_alt', true); ?>
+                            
                             <div class="swiper-slide">
-                                <?php $featured_image = get_the_post_thumbnail_url( get_the_ID(), 'large' ); ?>
-                                <?php $featured_image_alt = get_post_meta($featured_image , '_wp_attachment_image_alt', true); ?>
-
-                                <?php if ( ! empty( $featured_image ) ) : ?>
-                                    <a class="news-wrapper-featured" href="<?php echo get_permalink(); ?>">
-                                        <img src="<?php echo $featured_image; ?>" alt="<?php echo $featured_image_alt; ?>" title="<?php the_title(); ?>">
+                                <div class="latest-news-item">
+                                    <?php if ( ! empty( $featured_image ) ) : ?>
+                                        <a class="news-wrapper" href="<?php echo get_permalink(); ?>">
+                                            <img src="<?php echo $featured_image; ?>" alt="<?php echo $featured_image_alt; ?>" title="<?php the_title(); ?>">
+                                        </a>
+                                    <?php endif; ?>
+                                    <a href="<?php echo get_permalink(); ?>">
+                                        <h3><?php the_title(); ?></h3>
                                     </a>
-                                <?php endif; ?>
-
-                                <a href="<?php echo get_permalink(); ?>">
-                                    <h3><?php the_title(); ?></h3>
-                                </a>
-                                
-                                <p class="excerpt">
-                                    <?php the_excerpt(); ?>
-                                </p>
-                            </div>
+                                    <p class="excerpt">
+                                        <?php the_excerpt(); ?>
+                                    </p>
+                                    <a href="<?php the_permalink(); ?>" class="primary-button"><?php esc_html_e('Leia Mais', 'bulk'); ?></a>
+                                </div>
+                            </div>        
                         <?php endwhile; ?>
                     </div>
+
+                    <?php if ( get_field( 'enable_carousel_navigation' ) ) : ?>
+                        <div class="latest-news-urbs-navigation" data-animation>
+                            <button class="latest-news-urbs-navigation-next" aria-label="<?php esc_attr_e( 'Next Logo', 'bulk' ); ?>" title="<?php esc_attr_e( 'Next Logo', 'bulk' ); ?>">
+                                <?php theme_block_asset( 'img/nav-button-right.svg' ); ?>
+                            </button>
+                        </div>
+                    <?php endif; ?>
+
                 </div>
+
                 <?php if ( get_field( 'enable_carousel_pagination' ) ) : ?>
-                <div class="latest-news-urbs-pagination" role="presentation" data-animation></div>
-                <?php endif; ?>
-                <?php if ( get_field( 'enable_carousel_navigation' ) ) : ?>
-                    <div class="latest-news-urbs-navigation" data-animation>
-                        <button class="latest-news-urbs-navigation-prev" aria-label="<?php esc_attr_e( 'Previous Logo', 'bulk' ); ?>" title="<?php esc_attr_e( 'Previous Logo', 'bulk' ); ?>">
-                            <?php theme_block_asset( 'img/nav-button-left.svg' ); ?>
-                        </button>
-                        <button class="latest-news-urbs-navigation-next" aria-label="<?php esc_attr_e( 'Next Logo', 'bulk' ); ?>" title="<?php esc_attr_e( 'Next Logo', 'bulk' ); ?>">
-                            <?php theme_block_asset( 'img/nav-button-right.svg' ); ?>
-                        </button>
-                    </div>
+                    <div class="latest-news-urbs-pagination" role="presentation" data-animation></div>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
