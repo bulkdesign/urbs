ddad8bacd9a30369c5f03794025993d7d1ca8bdf
William Kunitake <william@bulkdesign.com.br>
2025-07-20 01:31:05 -0700

    Hiding meta values and share from Licitações

--- blocks/post-content/post-content.php
+++ blocks/post-content/post-content.php
@@ -35,10 +35,12 @@
                     <?php the_excerpt(); ?>
                 <?php endif; ?>
 
-                <div class="post-content-header-inner-content-meta">
-                    <p><?php esc_attr_e( 'Publicado em:', 'bulk' ); ?></p>
-                    <time><?php echo get_the_date('j F Y'); ?></time>
-                </div>
+                <?php if ( ! get_post_type() === 'licitacao' ) : ?>
+                    <div class="post-content-header-inner-content-meta">
+                        <p><?php esc_attr_e( 'Publicado em:', 'bulk' ); ?></p>
+                        <time><?php echo get_the_date('j F Y'); ?></time>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </header>
@@ -47,42 +49,45 @@
             <?php if( ! defined( 'REST_REQUEST' ) ) the_content(); ?>
         </div>
     </div>
-    <footer class="post-content-footer">
-        <div class="post-content-footer-inner">
-            <div class="post-content-footer-share">
-                <span>
-                    <?php esc_attr_e( 'Compartilhe esta notícia', 'bulk' ); ?>
-                </span>
-                
-                <ul class="post-content-share">
-                    <li>
-                        <a href="<?php the_permalink(); ?>" class="share-link share-to-clipboard" title="<?php esc_attr_e( 'Copy to clipboard', 'bulk' ); ?>">
-                            <?php theme_block_asset( '/img/icon-link.svg' ); ?>
-                            <?php theme_block_asset( '/img/icon-check.svg' ); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:?subject=<?php the_title(); ?>&body=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share via Email', 'bulk' ); ?>" class="share-link">
-                            <?php theme_block_asset( '/img/icon-email.svg' ); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share on LinkedIn', 'bulk' ); ?>" class="share-link">
-                            <?php theme_block_asset( '/img/icon-linkedin.svg' ); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://twitter.com/share?url=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share on X', 'bulk' ); ?>" class="share-link">
-                            <?php theme_block_asset( '/img/icon-x.svg' ); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share on Facebook', 'bulk' ); ?>" class="share-link">
-                            <?php theme_block_asset( '/img/icon-facebook.svg' ); ?>
-                        </a>
-                    </li>
-                </ul>
+
+    <?php if ( ! get_post_type() === 'licitacao' ) : ?>
+        <footer class="post-content-footer">
+            <div class="post-content-footer-inner">
+                <div class="post-content-footer-share">
+                    <span>
+                        <?php esc_attr_e( 'Compartilhe esta notícia', 'bulk' ); ?>
+                    </span>
+                    
+                    <ul class="post-content-share">
+                        <li>
+                            <a href="<?php the_permalink(); ?>" class="share-link share-to-clipboard" title="<?php esc_attr_e( 'Copy to clipboard', 'bulk' ); ?>">
+                                <?php theme_block_asset( '/img/icon-link.svg' ); ?>
+                                <?php theme_block_asset( '/img/icon-check.svg' ); ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:?subject=<?php the_title(); ?>&body=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share via Email', 'bulk' ); ?>" class="share-link">
+                                <?php theme_block_asset( '/img/icon-email.svg' ); ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share on LinkedIn', 'bulk' ); ?>" class="share-link">
+                                <?php theme_block_asset( '/img/icon-linkedin.svg' ); ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://twitter.com/share?url=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share on X', 'bulk' ); ?>" class="share-link">
+                                <?php theme_block_asset( '/img/icon-x.svg' ); ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" title="<?php esc_attr_e( 'Share on Facebook', 'bulk' ); ?>" class="share-link">
+                                <?php theme_block_asset( '/img/icon-facebook.svg' ); ?>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    <?php endif; ?>
 </article>
