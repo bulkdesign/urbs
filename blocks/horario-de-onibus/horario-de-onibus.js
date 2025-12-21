bf6e64734dec8800befd4b3a9a02562c46ea8e4d
William Kunitake <william@bulkdesign.com.br>
2025-12-21 07:41:59 -0500

    Fetch from REST API and populate dropdown with busses

---
+++ blocks/horario-de-onibus/horario-de-onibus.js
@@ -0,0 +1,47 @@
+(() => {
+    if (!document.querySelectorAll('.horario-de-onibus').length) return false;
+
+    const horarioWrapper = document.querySelectorAll('.horario-de-onibus');
+
+    horarioWrapper.forEach((horario) => {
+
+        const listaLinhas = async () => {
+            const linhasEndpoint = await fetch('/wp-json/urbs/v1/linhas');
+            const linhasJson = await linhasEndpoint.json();
+            const linhasData = linhasJson;
+            
+            const linhasSelect = horario.querySelector('select[name="horario-de-onibus-linhas"]');
+
+            linhasData.forEach((linha) => {
+                const linhasOption = document.createElement('option');
+                linhasOption.value = linha.codigo;
+                linhasOption.textContent = linha.linha;
+                linhasSelect.appendChild(linhasOption);
+            });
+
+            linhasSelect.addEventListener('change', async (linha) => {
+                const linhaSelecionada = linha.target.value;
+
+                if (!linhaSelecionada) return;
+
+                const horarioPontosEndpoint = await fetch(`/wp-json/urbs/v1/horarios-pontos?codigo_linha=${linhaSelecionada}`);
+                const horarioData = await horarioPontosEndpoint.json();
+
+                const listaHorarios = horario.querySelector('.horario-de-onibus-lista');
+
+                listaHorarios.innerHTML = '';
+
+                horarioData.forEach((todosHorarios) => {
+                    const listaItem = document.createElement('li');
+                    listaItem.textContent = todosHorarios.horario_tela;
+                    listaHorarios.appendChild(listaItem);
+                });
+
+            });
+        }
+
+        listaLinhas();
+
+    });
+
+})();
